622c54af4b540c34faaf8e59022fce5819243dee
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2024-01-12 01:18:52 -0800

    PostSchema model schema created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import { BodyParser } from "body-parser";
-import express from "express";
+// import routes
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+// routes
+app.use("/posts", postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://webmaestro:webmaestro@memorygram.4efcgca.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("server running on port " + PORT);
+    });
+  })
+  .catch((err) => console.log(err.message));
