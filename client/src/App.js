17e6474dccfa6dbc2bccb3980ce481f8a679fc8a
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2024-01-12 20:08:34 -0800

    frontend layout structure finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import memoriesImg from "./images/memories.png";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import { appBar, heading, image } from "./styles";
+
 const App = () => {
   return (
-    <div className="app">
-      <h1>Salom</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar sx={appBar} position="static" color="inherit">
+        <Typography sx={heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img sx={image} src={memoriesImg} alt="memories" height={60} />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+const appBar = {
+  borderRadius: 15,
+  margin: "30px 0",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const heading = {
+  color: "rgba(0,183,255, 1)",
+};
+
+const image = {
+  marginLeft: "15px",
+};
+
+export { image, heading, appBar };
